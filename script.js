e304f8a5f4e8c108f2b42b04fc0f575915cfe2d9
Khushi <khushinema02@gmail.com>
2024-07-18 23:25:07 +0530

    adding-navbar-effect

---
+++ script.js
@@ -0,0 +1,58 @@
+const body = document.body;
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+	const currentScroll = window.pageYOffset;
+	if (currentScroll <= 0) {
+		body.classList.remove("scroll-up");
+		return;
+	}
+
+	if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+		body.classList.remove("scroll-up");
+		body.classList.add("scroll-down");
+	} else if (
+		currentScroll < lastScroll &&
+		body.classList.contains("scroll-down")
+	) {
+		body.classList.remove("scroll-down");
+		body.classList.add("scroll-up");
+	}
+	lastScroll = currentScroll;
+});
+
+
+
+var menu = document.getElementById("menu");
+
+// menu.onclick = function () {
+// 	console.log('jk,km');
+	
+// }
+
+
+
+
+
+
+
+nav = document.querySelector("nav"),
+
+	modeToggle = document.querySelector(".theme"),
+	menu = document.querySelector(".menu");
+	close = document.querySelector(".openmenu");
+	links = document.querySelector(".links");
+
+
+	modeToggle.addEventListener("click", () => {
+		modeToggle.classList.toggle("active");
+		body.classList.toggle("dark");
+	})
+
+	
+
+	menu.onclick = function () {
+		console.log('jk,km');
+		links.classList.toggle("active");
+		menu.classList.toggle("openmenu");
+	}
